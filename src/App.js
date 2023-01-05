fb31b1c5b0a73c0cbffc85d96f1fa4f76b3aa657
Abdellah_Bounafaa <abdllahbounafaa@gmail.com>
2023-01-05 10:33:01 +0100

    github account finder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "./data/Data";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Account from "./components/Account";
 function App() {
+  const [username, setUsername] = useState("");
+  const [profile, setProfile] = useState([]);
+  const [error, setError] = useState("");
+  const handleName = (e) => {
+    setUsername(e.target.value);
+  };
+  const result = document.querySelector(".result");
+  const getProfile = async () => {
+    try {
+      const res = await axios.get(`users/${username}`);
+      setProfile(res.data);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="search-bar">
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => {
+            handleName(e);
+          }}
+        />
+        <button
+          onClick={() => {
+            getProfile();
+            setProfile([]);
+          }}
+          className="btn btn-primary p-2"
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </div>
+
+      <Account profile={profile} />
     </div>
   );
 }
